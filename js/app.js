142e799bdc64446658909467aae92761ca4927a5
Schoepfer <rudy.schoepfer@hotmail.fr>
2018-10-25 13:46:28 +0000

    ADDED - Button and Date Input

--- js/app.js
+++ js/app.js
@@ -1,50 +1,135 @@
-var app = new Vue({ 
-    el: '#app',
-    data: {
-        nameprojects:["github-ynov-vue"],
-        projects:[],
-        users:[],
-        tabProjects:[],
-    },
-    method:{
-        
-    },
-    mounted(){
-        fetch("https://api.github.com/search/repositories?q=github-ynov-vue",{
-            headers: {
-                "Authorization": "Basic cnVkeTg1MzA6QXUhMDIyODEwNTgyNA=="
-              },
-            method: "GET"
+var app = new Vue({
+  el: "#app",
+  data: {
+    selectProject: "",
+    nameUser: "",
+    dateDebut: "",
+    dateEnded: "",
+    name: "Vue.js",
+    nameprojects: ["Select project(s)", "github-ynov-vue"],
+    projects: [],
+    users: [
+      "mathiasLoiret",
+      "Mokui",
+      "etienneYnov",
+      "gfourny",
+      "mael61",
+      "alixnzt",
+      "mcourant",
+      "raphaelCharre",
+      "AlexDesvallees",
+      "ClementCaillaud",
+      "benjaminbra",
+      "Nair0fl",
+      "Killy85",
+      "msaintmartin",
+      "sfongue",
+      "rudy8530",
+      "Dakistos",
+      "Coblestone",
+      "BenoitCochet",
+      "thomaspich",
+      "TeofiloJ",
+      "maximerolland",
+      "LordInateur",
+      "KevinPautonnier",
+      "AntoineGOSSET"
+    ],
+    tabProjects: []
+  },
+  methods: {
+    reinit: function() {
+      //Récupère toutes les informations en fonction des noms de projet dans le tableau
+      this.nameprojects.forEach(projet => {
+        fetch("https://api.github.com/search/repositories?q=" + projet, {
+          headers: {
+            Authorization: "Basic cnVkeTg1MzA6QXUhMDIyODEwNTgyNA=="
+          },
+          method: "GET"
         })
-            .then(response =>response.json())
-            .then((data) => {
-                this.projects = data.items
-                this.projects.forEach((projet)=>{
-                    //Les commits,
-                    var monProjet = new Object();
-                    monProjet.login=projet.owner.login
-                    monProjet.url = projet.owner.url
-                    monProjet.nomProjet = projet.name
-                    monProjet.tab=[]
-                    fetch("https://api.github.com/repos/"+projet.full_name+"/commits",{
-                        headers: {
-                            "Authorization": ""
-                          },
-                        method: "GET"
-                    })
-                        .then(response =>response.json())
-                        .then((data)=>{
-                            var arrayCommits=[];
-                            data.forEach((response)=>{
-                                // var monCommit= new Object();
-                                // monCommit.message = response.commit.message
-                                monProjet.tab.push(response.commit.message)
-                            })
-                            monProjet.myCommits=arrayCommits
-                        })
-                        this.tabProjects.push(monProjet)
-                })
-                console.log(this.tabProjects)
-            })
+          .then(response => response.json())
+          .then(data => {
+            this.projects = data.items;
+            this.projects.forEach(projet => {
+              //Les commits,
+              var monProjet = new Object();
+              monProjet.login = projet.owner.login;
+              monProjet.url = projet.owner.html_url;
+              monProjet.nomProjet = projet.name;
+              monProjet.avatar = projet.owner.avatar_url;
+              monProjet.tab = [];
+              fetch(
+                "https://api.github.com/repos/" + projet.full_name + "/commits",
+                {
+                  headers: {
+                    Authorization: "Basic cnVkeTg1MzA6QXUhMDIyODEwNTgyNA=="
+                  },
+                  method: "GET"
+                }
+              )
+                .then(response => response.json())
+                .then(data => {
+                  data.forEach(response => {
+                    monProjet.tab.push(response.commit.message);
+                  });
+                });
+              this.tabProjects.push(monProjet);
+            });
+          });
+      });
+    },
+    search: function() {
+      var nameProject = this.selectProject;
+      var nameUser = this.selectUser;
+      var dateDebut = this.datebegin;
+      var dateFin = this.dateEnded;
+      
+      if (nameProject==""){
+        console.log("Vide project")
+      }
+      fetch("https://api.github.com/search/repositories?q=" + nameProject, {
+        headers: {
+          Authorization: "Basic cnVkeTg1MzA6QXUhMDIyODEwNTgyNA=="
+        },
+        method: "GET"
+      })
+        .then(response => response.json())
+        .then(data => {
+          this.projects = data.items;
+          this.projects.forEach(projet => {
+            if (projet.owner.login == nameUser) {
+              //Les commits,
+              var monProjet = new Object();
+              monProjet.login = projet.owner.login;
+              monProjet.url = projet.owner.html_url;
+              monProjet.nomProjet = projet.name;
+              monProjet.avatar = projet.owner.avatar_url;
+              monProjet.tab = [];
+              fetch(
+                "https://api.github.com/repos/" + projet.full_name + "/commits",
+                {
+                  headers: {
+                    Authorization: "Basic cnVkeTg1MzA6QXUhMDIyODEwNTgyNA=="
+                  },
+                  method: "GET"
+                }
+              )
+                .then(response => response.json())
+                .then(data => {
+                  var arrayCommits = [];
+                  data.forEach(response => {
+                    monProjet.tab.push(response.commit.message);
+                  });
+                });
+              this.tabProjects.push(monProjet);
+              console.log(monProjet);
+              return;
+            }
+          });
+        });
     }
+  },
+  mounted() {
+    this.reinit();
+  }
 });
